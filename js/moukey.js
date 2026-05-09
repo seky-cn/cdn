518c7ad38f3de1c225a5add2970db983d04eb911
MrWu-Blog <281120839@qq.com>
2026-05-09 18:23:07 +0800

    Create moukey.js

---
+++ js/moukey.js
@@ -0,0 +1,56 @@
+/*!
+https://seky.cn
+*/
+const devtools = {
+	isOpen: false,
+	orientation: undefined,
+};
+
+const threshold = 160;
+
+const emitEvent = (isOpen, orientation) => {
+	globalThis.dispatchEvent(new globalThis.CustomEvent('devtoolschange', {
+		detail: {
+			isOpen,
+			orientation,
+		},
+	}));
+};
+var getGlobal = function () {
+  if (typeof self !== 'undefined') { return self; }
+  if (typeof window !== 'undefined') { return window; }
+  if (typeof global !== 'undefined') { return global; }
+  throw new Error('unable to locate global object');
+};
+
+var globalThis = getGlobal();
+
+const main = ({emitEvents = true} = {}) => {
+	const widthThreshold = globalThis.outerWidth - globalThis.innerWidth > threshold;
+	const heightThreshold = globalThis.outerHeight - globalThis.innerHeight > threshold;
+	const orientation = widthThreshold ? 'vertical' : 'horizontal';
+
+	if (
+		!(heightThreshold && widthThreshold)
+		&& ((globalThis.Firebug && globalThis.Firebug.chrome && globalThis.Firebug.chrome.isInitialized) || widthThreshold || heightThreshold)
+	) {
+		if ((!devtools.isOpen || devtools.orientation !== orientation) && emitEvents) {
+			emitEvent(true, orientation);
+		}
+
+		devtools.isOpen = true;
+		devtools.orientation = orientation;
+	} else {
+		if (devtools.isOpen && emitEvents) {
+			emitEvent(false, undefined);
+		}
+
+		devtools.isOpen = false;
+		devtools.orientation = undefined;
+	}
+};
+
+main({emitEvents: false});
+setInterval(main, 500);
+
+export default devtools;
