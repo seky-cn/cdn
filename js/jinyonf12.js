1f4df8840b327e7beebaf23d7b74e5aa8bcabf87
MrWu-Blog <281120839@qq.com>
2026-05-09 18:19:51 +0800

    Create jinyonf12.js

---
+++ js/jinyonf12.js
@@ -0,0 +1,54 @@
+/* 禁用右键菜单并提醒 */
+document.oncontextmenu = function () {
+new Vue({
+    data:function(){
+        this.$notify({
+            title:"嘿！没有右键菜单",
+            message:"复制请用键盘快捷键",
+            position: 'bottom-right',
+            offset: 50,
+            showClose: false,
+            type:"warning"
+        });
+        return{visible:false}
+    }
+})
+return false;
+}
+/* 禁用F12按键并提醒 */
+document.onkeydown = function () {
+if (window.event && window.event.keyCode == 123) {
+  event.keyCode = 0;
+  event.returnValue = false;
+    new Vue({
+            data:function(){
+                this.$notify({
+                    title:"嘿！别瞎按",
+                    message:"坏孩子！打你屁屁哦！",
+                    position: 'bottom-right',
+                    offset: 50,
+                    showClose: false,
+                    type:"error"
+                });
+                return{visible:false}
+            }
+        })
+  return false;
+}
+};
+/* 复制提醒 */
+document.addEventListener("copy",function(e){
+    new Vue({
+        data:function(){
+            this.$notify({
+                title:"嘿！复制成功",
+                message:"若要转载请务必保留原文链接！爱你呦~",
+                position: 'bottom-right',
+                offset: 50,
+                showClose: false,
+                type:"success"
+            });
+            return{visible:false}
+        }
+    })
+})
